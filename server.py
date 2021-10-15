9b5df0c1ec8cee2519212c6defcf240253faad4a
Cindy Unrau <cindy4th@hotmail.com>
2021-10-14 17:03:37 -0700

    deployment test

--- server.py
+++ server.py
@@ -24,10 +24,10 @@ def handle_info():
     print("INFO")
     return {
         "apiversion": "1",
-        "author": "",  # TODO: Your Battlesnake Username
-        "color": "#888888",  # TODO: Personalize
-        "head": "default",  # TODO: Personalize
-        "tail": "default",  # TODO: Personalize
+        "author": "cindyunrau", 
+        "color": "#e389b9",  
+        "head": "default",  
+        "tail": "default", 
     }
 
 
